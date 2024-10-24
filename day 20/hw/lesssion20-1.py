7dd4a49c7de87fa365351c182fa1910e74927871
wiklauri27giorgi <gtsiklauri27@gmail.com>
2024-10-24 20:54:26 +0400

    second commit

---
+++ day 20/hw/lesssion20-1.py
@@ -0,0 +1,15 @@
+number=10
+number=15
+if number is 15:
+   print( "10<15")
+else:
+   print("15>10")
+   
+   print("15=10")
+
+   number=3
+   number=2
+if number is 3:
+      print("you won 100,000")
+else:
+    print("you won 10,000")
